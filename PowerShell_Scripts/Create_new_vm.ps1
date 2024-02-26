718639c642b1fb1ff046c03aca6a3cf79e5acc1c
Marcus Egelius <marcus.egelius@yh.nackademin.se>
2024-02-26 02:20:13 -0800

    Differencingdisk

--- PowerShell_Scripts/Create_new_vm.ps1
+++ PowerShell_Scripts/Create_new_vm.ps1
@@ -1,18 +1,15 @@
 ﻿#Set these variables for your VM & VHD’s
-$VMName= “SRVDC”
+$VMName= “SRVDC1”
 $RAM= 8GB
 $SwitchName= “Internet”
 $CPUCount= 2
-$VHDPath= “C:\Production\SRVDC-C.vhdx”
 $VHDSize= 140GB
-$DataVHDPath= “C:\Production\SRVDC-E.vhdx”
+$MotherVHD = “C:\Production\Motherdisk.vhdx”
+$DataVHDPath= “C:\Production\$VMName.vhdx”
 $DataVHDSize= 400GB
 
 #Deploy the new virtual machine
-New-VM -NewVHDPath $VHDPath -NewVHDSizeBytes $VHDSize -Generation 1 -MemoryStartupBytes $RAM -Name $VMName -SwitchName $SwitchName
+New-VHD -ParentPath $MotherVHD -Path $DataVHDPath -Differencing
+New-VM -VHDPath $DataVHDPath -MemoryStartupBytes $RAM -Name $VMName -SwitchName $SwitchName
 Set-VM -Name $VMName -ProcessorCount $CPUCount
 Set-VMMemory $VMName -DynamicMemoryEnabled $true
-
-#Add a VHD for file/data partition
-New-VHD -Path $DataVHDPath -SizeBytes $DataVHDSize -Dynamic
-Add-VMHardDiskDrive –ControllerType SCSI -ControllerNumber 0 -VMName $VMName -Path $DataVHDPath
