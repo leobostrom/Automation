52a46b3af181ac646fa9350ae1d8cfefbeeec76c
Marcus Egelius <marcus.egelius@yh.nackademin.se>
2024-02-26 01:38:21 -0800

    k

--- PowerShell_Scripts/Create_new_vm.ps1
+++ PowerShell_Scripts/Create_new_vm.ps1
@@ -11,6 +11,7 @@ $DataVHDSize= 400GB
 #Deploy the new virtual machine
 New-VM -NewVHDPath $VHDPath -NewVHDSizeBytes $VHDSize -Generation 1 -MemoryStartupBytes $RAM -Name $VMName -SwitchName $SwitchName
 Set-VM -Name $VMName -ProcessorCount $CPUCount
+Set-VMMemory $VMName -DynamicMemoryEnabled $true
 
 #Add a VHD for file/data partition
 New-VHD -Path $DataVHDPath -SizeBytes $DataVHDSize -Dynamic
