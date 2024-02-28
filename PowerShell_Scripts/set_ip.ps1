07d646bbc82f6498ee1e53d5aee08e0604f495fd
leo boström <leo.bostrom@yh.nackademin.se>
2024-02-28 15:39:01 -0600

    k

--- PowerShell_Scripts/set_ip.ps1
+++ PowerShell_Scripts/set_ip.ps1
@@ -1,18 +1,16 @@
-$VMName= "SRVDC"
-$IPAdd= ”192.168.70.21"
-$Gateway= ”192.168.70.1"
-$DNSAdd= ”192.168.70.4"
+$VMName = "bajs"
+$IPAdd = "192.168.70.21"
+$Gateway = "192.168.70.1"
+$DNSAdd = "192.168.70.4"
 
 Invoke-Command -VMName $VMName -ScriptBlock {
+    # Set time zone, rename computer, install the Essentials role
+    $TimeZone= "Central Standard Time"
+    $VMName= "bajs"
+    TZUtil /s $TimeZone
+    Rename-Computer -NewName $VMName -Confirm:$False
 
-#Set time zone, rename computer, install the Essentials role
-$TimeZone= “Central Standard Time”
-$VMName= "SRVDC"
-TZUtil /s $TimeZone
-Rename-Computer -NewName $VMName -Confirm:$False
-
-#Configure the network settings
-New-NetIPAddress -InterfaceAlias “Ethernet” -IPAddress $IPAdd -PrefixLength 24 -DefaultGateway $Gateway
-Set-DnsClientServerAddress -InterfaceAlias “Ethernet” -ServerAddresses $DNSAdd
-
+    # Configure the network settings
+    New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress $using:IPAdd -PrefixLength 24 -DefaultGateway $using:Gateway
+    Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $using:DNSAdd
 }
